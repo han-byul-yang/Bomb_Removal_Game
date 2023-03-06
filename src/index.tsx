f48222b58dbbad93210a85cd3afc712242e9afd5
Han Byul <yahb9@naver.com>
2023-03-06 21:50:15 +0900

    Init: 초기 세팅

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import App from 'routes'
+
+import './styles/index.scss'
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+const queryClient = new QueryClient()
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import { lazy } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return <Routes />
+}
+
+export default App
